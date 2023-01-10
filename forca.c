c4d9ddc65f4165c7e5f2b70263df3e6754357fd1
allanmori21 <allanmori@hotmail.com>
2023-01-10 18:30:44 -0300

    Colocando o loop do-while no código

---
+++ forca.c
@@ -0,0 +1,16 @@
+#include <studio.h>
+
+int main () {
+    char palavrasecreta [20];
+
+    sprintf (palavrasecreta, "MELANCIA");
+
+    printf("%s", palavrasecreta);
+
+    int acertou = 0;
+    int enforcou = 0;
+
+    do {
+
+    } while (!acertou && !enforcou);
+}

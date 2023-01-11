e8e43266540b441903c85b9f76196d3831427027
allanmori21 <allanmori@hotmail.com>
2023-01-11 09:57:39 -0300

    Laços encadeados no código

--- forca.c
+++ forca.c
@@ -9,14 +9,37 @@ int main () {
     int acertou = 0;
     int enforcou = 0;
 
+    char chutes [26];
+    int tentativas = 0;
+
     do {
+        for (int i = 0; i < strlen(palavrasecreta); i++){
+            int achou = 0;
+            for (int j = 0; j < tentativas; j++){
+                if (chutes[j] == palavrasecreta[i]){
+                    achou = 1;
+                    break;
+                }
+            }
+            if (achou){
+                 printf("%c ", palavrasecreta[i]);
+            } else {
+                 printf("_ ");
+            }
+           
+        }
+        printf("\n");
+
         char chute;
-        printf ("Qual letra?: ");
-        scanf ("%c", &chute);
+        printf ("Adivinha uma letra?: ");
+        scanf (" %c", &chute);
+
+        chutes[tentativas] = chute;
+        tentativas++;
 
         for (int i = 0; i < strlen(palavrasecreta); i++){
             if(palavrasecreta[i] == chute){
-                printf("A posição %d tem essa letra!\n", i);
+                printf("A posicao %d tem essa letra!\n", i);
            }
          }
     } while (!acertou && !enforcou);
